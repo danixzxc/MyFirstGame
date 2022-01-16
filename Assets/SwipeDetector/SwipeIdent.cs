c32635017f781829d74f958187b263713352c764
danixzxc <daniil.grishel@gmail.com>
2022-01-16 22:34:40 +0300

    anotherVersion

---
+++ Assets/SwipeDetector/SwipeIdent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeIdent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* String[] Direction = new String[4];
+        Direction[1] = "Up";
+        Direction[2] = "Down";
+        Direction[3] = "Left";
+        Direction[4] = "Right";
+        SwipeData obj;
+        if (obj.Direction == Up)
+        {
+
+        } */
+    }
+}
